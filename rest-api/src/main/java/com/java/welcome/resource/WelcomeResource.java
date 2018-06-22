770147214d5434f70e8ce29c3689d777e4bedd6e
Valter Filho <valter.filho@ymail.com>
2018-06-22 14:02:12 -0300

    Inclusão do produces no retorno da chamada welcome

--- rest-api/src/main/java/com/java/welcome/resource/WelcomeResource.java
+++ rest-api/src/main/java/com/java/welcome/resource/WelcomeResource.java
@@ -2,12 +2,15 @@ package com.java.welcome.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("welcome")
 public class WelcomeResource {
 
 	
 	@GET
+	@Produces(MediaType.TEXT_HTML)
 	public String welcome() {
 		return "Welcome";
 	}
